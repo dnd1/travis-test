6813c27f4b7acd2e250cf5c7dbdf59b19699d2d5
Daniel Rodriguez del Villar Trimarchi <daniel@dnd1.com>
2021-05-14 14:07:01 -0400

    added image tag variable to ci script

--- push-to-registry.sh
+++ push-to-registry.sh
@@ -1,5 +1,5 @@
 REPO_NAME=travis-test
-IMAGE_TAG=$TRAVIS_COMMIT
+IMAGE_TAG=latest
 
 REPO_URL="208369778572.dkr.ecr.us-east-1.amazonaws.com"
 
@@ -14,7 +14,7 @@ aws ecr get-login-password --region us-east-1| docker login --username AWS --pas
 echo "running docker build"
 docker build -t ${REPO_NAME} . 
 docker tag "${IMAGE_NAME}" "${REPO_URL}/${IMAGE_NAME}:${IMAGE_TAG}"
-docker tag "${REPO_NAME}:latest" "${REPO_URL}/${REPO_NAME}:latest"
+docker tag "${REPO_NAME}:${IMAGE_TAG}" "${REPO_URL}/${REPO_NAME}:${IMAGE_TAG}"
 
 docker push "${REPO_URL}/${IMAGE_NAME}:${IMAGE_TAG}"
-docker push "${REPO_URL}/${REPO_NAME}:latest"
+docker push "${REPO_URL}/${REPO_NAME}:${IMAGE_TAG}"
