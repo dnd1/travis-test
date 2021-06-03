ba21dd234a5ddbd092932cf3a4489c569f3d064d
Daniel Rodriguez del Villar Trimarchi <daniel@dnd1.com>
2021-06-03 13:58:21 -0400

    Updated with travis stuff

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ exports.handler = async function (event, context) {
   var logger = new FunnyLogger()
   logger.haha('Hello World!') //☜(ﾟヮﾟ☜) Hello World!
 
-  console.log('Hello Github!!!')
+  console.log('Hello Travis!!!')
   console.log('ENVIRONMENT VARIABLES\n' + JSON.stringify(process.env, null, 2))
   console.log('EVENT\n' + JSON.stringify(event, null, 2))
-  console.log('Bye Bye Cruel World!!!')
+  console.log('Travis is the worst of all CI/CD platforms!!!')
   return context.logStreamName
 }
